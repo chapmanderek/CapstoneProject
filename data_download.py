72e9f7153ff79a624bd227b859d948d1b3df2517
Derek Chapman <derekachapman@gmail.com>
2016-11-21 10:15:36 -1000

    Add Election Results by state csv, data download nows stores info as simple file seperated by colon instead of as a json structure.

--- data_download.py
+++ data_download.py
@@ -5,13 +5,13 @@ print "Fetching State ID #s..."
 state_json = requests.get("https://api.datausa.io/attrs/geo/?sumlevel=state").json()
 state_data = state_json['data']
 counter = 0
-state_id_dict = dict()
+state_id_file = open("state_ids.txt", 'w')
+next_line = ""
 for each_state in state_data:
 	counter += 1
-	state_id_dict[each_state[9]] = each_state[8]
-
-state_id_file = open("state_ids.txt", 'w')
-state_id_file.write(str(state_id_dict))
+	next_line = each_state[9] + ":" + each_state[8] + "\n"
+	state_id_file.write(next_line)
+state_id_file.close()
 print "Retrieved {0} state IDs".format(counter)
 
 # put wage info for each state into the database
@@ -22,11 +22,26 @@ wage_json = requests.get(wage_url).json()
 wage_data = wage_json['data']
 
 wage_counter = 0
-wage_dict = dict()
+wage_data_file = open("wage_data.txt", 'w')
 for each_state_wage in wage_data:
 	wage_counter += 1
-	wage_dict[each_state_wage[2]] = each_state_wage[1]
-
-wage_data_file = open("wage_data.txt", 'w')
-wage_data_file.write(str(wage_dict))
+	next_line = each_state_wage[1] + ":" + str(each_state_wage[2]) + '\n'
+	wage_data_file.write(next_line)
+wage_data_file.close()
 print "Retrieved {0} records of wage data".format(wage_counter)
+
+# put education info for each state into the database
+# wage headers': [u'year', u'geo', u'% people with some college']
+print "Fetching education data..."
+edu_url = "https://api.datausa.io/api/?show=geo&sumlevel=state&required=some_college"
+edu_json = requests.get(edu_url).json()
+edu_data = edu_json['data']
+
+edu_counter = 0
+edu_data_file = open("edu_data.txt", 'w')
+for each_state_edu in edu_data:
+	if each_state_edu[0] != 2015 : continue
+	edu_counter += 1
+	next_line = each_state_edu[1] + ':' + str(each_state_edu[2]) + '\n'
+	edu_data_file.write(next_line)
+print "Retrieved {0} records of edu data".format(edu_counter)
--- data_load_SQL.py
+++ data_load_SQL.py
@@ -1,21 +1,33 @@
 import sqlite3
+import json
 
 # create and open a new sql database to house data
 sql_connection = sqlite3.connect('state_info.sqlite')
 cur = sql_connection.cursor()
-cur.execute('DROP TABLE StateInfo')
+cur.execute('DROP TABLE IF EXISTS StateInfo')
 cur.execute('''CREATE TABLE StateInfo 
 	(id INTEGER PRIMARY KEY, name TEXT UNIQUE, id_num INTEGER UNIQUE, avg_wage_2014 INTEGER)''')
 
-print "Loading State ID #s"
-for each_state in state_data:
-	cur.execute('INSERT OR IGNORE INTO StateInfo (name, id_num) VALUES (?, ?)', (each_state[9], each_state[8]))
+# Load state ids into sql database
+print "Loading State ID #s..."
+counter = 0
+state_handle = open("state_ids.txt")
+for each_state in state_handle:
+	each_state_parts = each_state.split(":")
+	counter += 1
+	cur.execute('INSERT OR IGNORE INTO StateInfo (name, id_num) VALUES (?, ?)', (each_state_parts[0], each_state_parts[1]))
 sql_connection.commit()
+print "Loaded {0} state IDs".format(counter)
 
+# Load wage data into sql database
 print "Loading Wage Data"
-for each in wage_data:
-	cur.execute('UPDATE StateInfo SET avg_wage_2014 = ? WHERE id_num = ?', (each[2], each[1]))
-	# print each[1], " : ", each[2]
+counter = 0
+wage_data = open("wage_data.txt")
+for each_wage in wage_data:
+	counter += 1
+	each_wage_parts = each_wage.split(":")
+	cur.execute('UPDATE StateInfo SET avg_wage_2014 = ? WHERE id_num = ?', (each_wage_parts[2], each_wage_parts[1]))
 sql_connection.commit()
+print "Loaded  wage data for {0} states".format(counter)
 
 cur.close()
